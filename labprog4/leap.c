40145728960bc8a9e41944b4589d8fa3df2c860c
mishrazishani <zimi16ec@cmrit.ac.in>
2016-09-12 23:58:55 +0530

    Update and rename main.c to leap.c

---
+++ labprog4/leap.c
@@ -0,0 +1,25 @@
+/*to read year and check if leap year or not*/
+#include<stdio.h>
+int main()
+{
+int year;
+printf("enter a year\n");
+scanf("%d",&year);
+if(year%400==0)
+  {
+  printf("%d is a leap year\n",year);
+  }
+else if(year%100==0)
+  {
+  printf("%d is not a leap year\n",year);
+  }
+else if(year%4==0)
+  {
+  printf("%d is a leap year\n",year);
+  }
+else
+  {
+  printf("%d is not a leap year\n",year);
+  }
+return 0;
+}
