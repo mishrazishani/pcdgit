4ef642517d5fd1527d5e3b698b60e7497f0244a8
mishrazishani <zimi16ec@cmrit.ac.in>
2016-09-12 23:28:29 +0530

    Update and rename main.c to reverse.c

---
+++ labprog2/reverse.c
@@ -0,0 +1,32 @@
+/*to find the reverse of a number and check if palindrome*/
+#include<stdio.h>
+int reverse(int x)
+{
+    int y;
+    int rev=0;
+    y=x;
+while(y!=0)
+{
+ rev=rev*10;
+ rev=rev+(y%10);
+ y=y/10;
+ }
+return rev;
+}
+int main()
+  {
+  int x,b;
+  printf("enter the number\n");
+  scanf("%d",&x);
+  b=reverse(x);
+  printf(" the reverse of the number is %d\n",b);
+  if(y==rev)
+    {
+    printf("the number is a palindrome\n");
+    }
+  else
+    {
+    printf("the number is not a palindrome\n");
+    }
+return 0;
+}
