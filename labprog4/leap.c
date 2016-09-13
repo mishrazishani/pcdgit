5b67c75c79114757902f54adbdf8717f6cc6ba8f
mishrazishani <zimi16ec@cmrit.ac.in>
2016-09-13 16:03:57 +0530

    Update leap.c

--- labprog4/leap.c
+++ labprog4/leap.c
@@ -1,14 +1,23 @@
 /*to read year and check if leap year or not*/
 #include<stdio.h>
 int ly(int y);
+
+ int ly(y)
+ {
+    if(((y%4==0)&&(y%100!=0))||(y%400==0))
+    return 0;
+    else
+    return 1;
+ }
+
 int main()
 {
-  int y,x;
+  int x ,y;
   printf("enter the year  :");
   scanf("%d",&y);
   x=ly(y);
   if(x==0)
-  { 
+  {
     printf("it is a leap year\n");
   }
   else if(x==1)
@@ -17,10 +26,4 @@ int main()
   }
   return 0;
 }
-int ly(y)
-{
-  if(((y%4==0)&&(y%100!=0))||(y%400==0))
-    return 0;
-  else
-    return 1;
-}
+
