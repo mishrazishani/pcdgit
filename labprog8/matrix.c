24c1b3d55e1a1a6910874c78267e13066718e880
mishrazishani <zimi16ec@cmrit.ac.in>
2016-09-28 19:51:54 +0530

    Update matrix.c

--- labprog8/matrix.c
+++ labprog8/matrix.c
@@ -1,37 +1,31 @@
 #include<stdio.h>
-int mult(int m, int n, int a[m][n],int p, int q, int b[p][q], int c[m][q])
+void mult(int m, int n, int a[m][n],int p, int q, int b[p][q], int i, int j, int c[i][j])
 {
-int i,j, k;
+int k;
 for(i=0;i<m;i++)
 	{
 	for(j=0;j<q;j++)
-		{
-		c[i][j]=0;	
 		
-	for(k=0;k<p;k++)
+		c[i][j]=0;	
+		for(k=0;k<n;k++)
 		{	
-		c[i][j]= c[i][j] +( a[i][k]*b[k][j]);
+		c[i][j]+=( a[i][k]*b[k][j]);
         	}
-		}
+		
 	}
-return c[i][j];
 }
 int main()
 {
-int temp,m, n, p,q, i,j,k;
-int a[10][10],b[10][10],c[10][10]; 
-printf("enter the order of matrix A\n");
-scanf("%d%d", &m,&n);
-printf("enter the order of matrix B\n");
-scanf("%d%d", &p,&q);
+int  m,n,p,q,i,j,k,a[10][10],b[10][10],c[10][10];
+printf("enter the order of the matrix A\n");
+scanf("%d%d",&m,&n);
+printf("enter the order of the matrix B\n");
+scanf("%d%d",&p,&q);
 if(n!=p)
 	{
-	printf("matrix multiplication not possible\n");
+	printf("matrix multiplication is not possible\n");
 	}
-else
-{
-printf("enter the elements of matrix A\n");
-}
+printf("enter the elements into A\n");
 for(i=0;i<m;i++)
 	{
 	for(j=0;j<n;j++)
@@ -39,43 +33,41 @@ for(i=0;i<m;i++)
 		scanf("%d",&a[i][j]);
 		}
 	}
-
-printf("enter the elements of matrix B\n");
+printf("enter the elements into matrix B\n");
 for(i=0;i<p;i++)
 	{
-	for(j=0;j<q;j++)
+	for(j=0;j<n;j++)
 		{
 		scanf("%d",&b[i][j]);
 		}
 	}
 printf("the elements of matrix A are\n");
 for(i=0;i<m;i++)
-{
-for(j=0;j<n;j++)
-{
-printf("%d\n",a[i][j]);
-}
-}
-printf("the elemnts of matrix B are\n");
+	{
+	for(j=0;j<n;j++)
+		{
+		printf("%3d",a[i][j]);
+		}
+	printf("\n");
+	}
+printf(" the elements of matrix B are\n");
 for(i=0;i<p;i++)
-{
-for(j=0;j<q;j++)
-{
-printf("%d\n",b[i][j]);
-}
-}
-//multiplication
-temp=mult(m,n,a,p,q,b,c);
-printf("the resultant matrix C is\n");
+	{
+	for(j=0;j<q;j++)
+		{
+		printf("%3d",b[i][j]);
+		}
+	printf("\n");
+	}
+mult(m,n,a,p,q,b,i,j,c);
+printf("product of the matrices are\n");
 for(i=0;i<m;i++)
-{
-for(j=0;j<n;j++)
-{
-printf("%d\n",c[i][j]);
-}
-printf("\n");
-}
+	{
+	for(j=0;j<q;j++)
+		{
+		printf("%3d",c[i][j]);
+		}
+	printf("\n");
+	}
 return 0;
 }
-	
-
