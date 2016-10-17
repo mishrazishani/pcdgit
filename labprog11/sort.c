da75c56b90fcf0421936b7cff6b98ca0dd6158ac
mishrazishani <zimi16ec@cmrit.ac.in>
2016-10-17 19:29:07 +0530

    Update and rename main.c to sort.c

---
+++ labprog11/sort.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include<string.h>
+void inputstrings(int m, int n, char s[m][n])
+{
+for(int i=0;i<m;i++)
+	{
+	printf("enter next string\n");
+	scanf("%s",s[i]);
+	}
+}
+void outputstrings(int m, int n, char s[m][n])
+{
+for(int i=0;i<m;i++)
+{
+printf("%s\n",s[i]);
+}
+}
+void sort(int m, int n,char s[m][n])
+{
+int flag=0;
+char temp[n];
+int t=0;
+for(int i=0;i<m-1,j++)
+{
+	flag=0;
+	for(int j=0;j<m-i-1;j++)
+	{
+	t=strcmp(s[j],s[j+1]);
+	if(t>0)
+		{
+		strcpy(temp,s[j]);
+		strcpy(s[j],s[j+1]);
+		strcpy(s[j+1],temp);
+		flag=1;
+		}
+	}
+	if(flag==0)
+	{
+	break;
+	}
+}
+}
+int search(int m, int n, char s[m][n], char *key)
+{
+int left=0;
+int right=m;
+int mid;
+int t;
+while(left<right)
+	{
+	mid=left+right/2;
+	t=strcmp(s[mid],key);
+	if(t<0)
+		{
+		left=mid+1;
+		}
+	if(t>0)
+		{
+		right=mid-1;
+		}
+	else
+	{
+	return mid;
+	}
+}
+return -1;
+}
+
+int main()
+{
+int m;
+int n=100;
+printf("enter the no of strings\n");
+scanf("%d",&m);
+char strings[m][n];
+intputstrings(m,n,strings);
+sort(m,n,strings);
+outputstrings(m,n,strings);
+int r;
+char key[20];
+printf("enter the key to be searchrd\n");
+scanf("%s",key);
+r=search(m,n,strings,key);
+if(r>0)
+	{
+	printf("the position of the key is %d\n",r);
+	}
+else
+	{
+	printf("key is not found\n");
+	}
+return 0;
+}
