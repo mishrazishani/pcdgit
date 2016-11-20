516be4a9b06ee25eea0302d782d24ae3b6ac35ef
mishrazishani <zimi16ec@cmrit.ac.in>
2016-11-20 23:52:26 +0530

    Update main.c

--- labprog1/main.c
+++ labprog1/main.c
@@ -14,15 +14,15 @@ int main()
   }
   if(d>0)
   {
-    r1=(-b+sqrt(d)/(2*a);
-    r2=(-b-sqrt(d)/(2*a);
+    r1=(-b+sqrt(d)/(2*a));
+    r2=(-b-sqrt(d)/(2*a));
     printf("roots are real, r1=%f, r2=%f\n",r1,r2);
   }
   else
   {
     rr=-b/(2*a);
     ir=sqrt(-d)/(2*a);
-    printf("the roots are complex r1=%f+i%f, r2=%f-i%f\n,rr,ir,rr,ir");
+    printf("the roots are complex r1=%f+i%f, r2=%f-i%f\n",rr,ir,rr,ir);
   }
   return 0;
   }
